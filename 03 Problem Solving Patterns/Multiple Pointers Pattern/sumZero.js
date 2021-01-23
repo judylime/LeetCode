8fdda942780e5408f92dfadd80a161bff192cdab
Judy Lian <judylime9@gmail.com>
2021-01-22 16:42:55 -0800

    Create basic Translator

--- 03 Problem Solving Patterns/Multiple Pointers Pattern/sumZero.js
+++ 03 Problem Solving Patterns/Multiple Pointers Pattern/sumZero.js
@@ -31,4 +31,4 @@ function sumZero(arr){
   }
 }
 
-sumZero([-4,-3,-2,-1,0,1,2,3,10])
+sumZero([-4,-3,-2,-1,0,1,2,3,10]) ud e m
---
+++ BasicTranslator.py
@@ -0,0 +1,20 @@
+'''
+Giraffe Language
+vowels -> g
+dog -> dgg
+cat -> cgt
+'''
+
+def translate (phrase):
+  translation = ""
+  for letter in phrase:
+    if letter.lower() in "aeiou":
+      if letter.isupper():
+        translation = translation + "G"
+      else:
+        translation = translation + "g"
+        
+    else:
+      translation = translation + letter
+  return translation
+print(translate(input("Enter a phrase:")))
